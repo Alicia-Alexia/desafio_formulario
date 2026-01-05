f52669c3b8c321d59650a316ede583f7a28e3595
Alicia <alicia.alexia03@gmail.com>
2026-01-05 16:29:23 -0300

    fix: corrige nomes de funcoes e adiciona reset do formulario ao ter sucesso

--- script.js
+++ script.js
@@ -5,14 +5,13 @@ const mensagemSucesso = document.getElementById('mensagem-sucesso');
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+    mensagemSucesso.classList.add('hidden');
     validarFormulario();
 });
 
 function validarFormulario() {
     let formValido = true;
 
-    // --- Validação do Nome ---
-    // .trim() remove espaços em branco antes e depois (evita que só "espaço" conte como nome)
     if (nomeInput.value.trim() === '') {
         definirErro(nomeInput, 'O nome é obrigatório.');
         formValido = false;
@@ -20,30 +19,48 @@ function validarFormulario() {
         definirSucesso(nomeInput);
     }
 
-    // --- Validação do E-mail ---
     if (emailInput.value.trim() === '') {
         definirErro(emailInput, 'O e-mail é obrigatório.');
         formValido = false;
-    } else {
+    } 
+    else if (!isEmailValido(emailInput.value)) {
+        definirErro(emailInput, 'Digite um e-mail válido (ex: .com, .br)');
+        formValido = false;
+    } 
+    else {
         definirSucesso(emailInput);
     }
 
     if (formValido) {
         mensagemSucesso.classList.remove('hidden');
-         form.reset(); 
+        form.reset();
+        limparEstilosVisuais();
+
+        console.log("Sucesso! Formulário limpo.");
     }
 }
 
-// Funções auxiliares para mostrar/esconder erros visualmente
 function definirErro(input, mensagem) {
     const inputGroup = input.parentElement;
     const small = inputGroup.querySelector('.error-text');
-
+    
     small.innerText = mensagem;
-    inputGroup.className = 'input-group error';
+    inputGroup.className = 'input-group error'; 
 }
 
 function definirSucesso(input) {
     const inputGroup = input.parentElement;
     inputGroup.className = 'input-group'; 
+}
+
+function isEmailValido(email) {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+}
+
+function limparEstilosVisuais() {
+    const inputGroups = document.querySelectorAll('.input-group');
+    inputGroups.forEach((grupo) => {
+        grupo.className = 'input-group';
+    });
 }
